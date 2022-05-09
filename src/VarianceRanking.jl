0e2baf4dd0ad52178f381651d7aa96892e43fa89
PatrikCavina <patrik.cavina@edu.unife.it>
2022-05-09 14:57:31 +0200

    VarianceRanking implementation

---
+++ src/VarianceRanking.jl
@@ -0,0 +1,137 @@
+struct VarianceRanking <: AbstarctFilterBased
+    k::Int64
+
+    function VarianceRanking(k::Int64)
+        @assert k >= 0 "k must be greater or equal 0"
+        new(k)
+    end
+end
+
+selector_k(selector::VarianceRanking) = selector.k
+selector_rankfunct(selector::VarianceRanking) = StatsBase.var
+
+function apply(
+    mfd::SoleBase.AbstractMultiFrameDataset,
+    selector::VarianceRanking;
+    normalize_function=nothing
+)
+    mfd_clone = deepcopy(mfd)
+    apply!(mfd_clone, selector; normalize_function=normalize_function)
+    return mfd_clone
+end
+
+function apply(
+    mfd::SoleBase.AbstractMultiFrameDataset,
+    selector::VarianceRanking,
+    frame_index::Integer;
+    normalize_function=nothing
+)
+    mfd_clone = deepcopy(mfd)
+    apply!(mfd_clone, selector, frame_index; normalize_function=normalize_function)
+    return mfd_clone
+end
+
+function apply(
+    mfd::SoleBase.AbstractMultiFrameDataset,
+    selector::VarianceRanking,
+    frame_indices::AbstractVector{<:Integer};
+    normalize_function=nothing
+)
+    mfd_clone = deepcopy(mfd)
+    for i in frame_indices
+        apply!(mfd_clone, selector, i, normalize_function=normalize_function)
+    end
+    return mfd_clone
+end
+
+function apply!(
+    mfd::SoleBase.AbstractMultiFrameDataset,
+    selector::VarianceRanking;
+    normalize_function=nothing
+)
+    df = SoleBase.SoleDataset.data(mfd)
+    @assert all(col->(col isa Union{Array{<:Number},Number}),
+                collect(Iterators.flatten(eachcol(df))))
+                    "Attributes are not numerical type"
+
+    if !isnothing(normalize_function)
+        df_norm = normalize_function(df)
+        bm = build_bitmask(df_norm, selector)
+    else
+        bm = build_bitmask(df, selector)
+    end
+
+    indicies = findall(x->!x, bm)
+    SoleBase.SoleDataset.dropattributes!(mfd, indicies)
+end
+
+function apply!(
+    mfd::SoleBase.AbstractMultiFrameDataset,
+    selector::VarianceRanking,
+    frame_index::Integer;
+    normalize_function=nothing
+)
+    # frame from 'frame_index'
+    fr = SoleBase.frame(mfd, frame_index)
+    @assert all(col->(col isa Union{Array{<:Number},Number}),
+                collect(Iterators.flatten(eachcol(fr))))
+                    "Attributes are not numerical type"
+
+    # frame indicies
+    fr_indicies = SoleBase.SoleDataset.frame_descriptor(mfd)[frame_index]
+
+    # check if the frame needs normalization
+    if !isnothing(normalize_function)
+        fr_norm = normalize_function(fr)
+        fr_bm = build_bitmask(fr_norm, selector)
+    else
+        fr_bm = build_bitmask(fr, selector)
+    end
+
+    # bit mask for entire dataset
+    bm = trues(nattributes(mfd))
+    for i in 1:nattributes(fr)
+        bm[fr_indicies[i]] = fr_bm[i]
+    end
+
+    indicies = findall(x->!x, bm)
+    SoleBase.SoleDataset.dropattributes!(mfd, indicies)
+end
+
+function apply!(
+    mfd::SoleBase.AbstractMultiFrameDataset,
+    selector::VarianceRanking,
+    frame_indices::AbstractVector{<:Integer};
+    normalize_function=nothing
+)
+    for i in frame_indices
+        apply!(mfd, selector, i, normalize_function=normalize_function)
+    end
+end
+
+function build_bitmask(df::AbstractDataFrame, selector::VarianceRanking)::BitVector
+    ranks = map(x->(x[1], selector_rankfunct(selector)(collect(Iterators.flatten(x[2])))), enumerate(eachcol(df)))
+
+    # TODO: improve NaN management
+    function lt(x, y)
+        if isnan(x) && !isnan(y)
+            return true
+        elseif !isnan(x) && isnan(y)
+            return false
+        end
+        return x < y
+    end
+    sort!(ranks, by=x->x[2], lt=lt)
+
+    n_cols = ncol(df)
+    if selector_k(selector) < n_cols
+        bm = falses(n_cols)
+        lower = n_cols - selector_k(selector) + 1
+        for r in ranks[lower:n_cols]
+            bm[r[1]] = true
+        end
+    else
+        bm = trues(n_cols)
+    end
+    return bm
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,20 +6,69 @@ include("./test_function.jl")
 
 @testset "SoleFeatures.jl" begin
 
+    df = fake_temporal_series_dataset()
+
     @testset "Testing VarianceThreshold on temporal series in MultiFrameDataset" begin
         # test VarianceThreshold on temporal series in MultiFrameDataset
         # Variance threshold: 0.09
-        # Expected behavior: Attributes "firstcol" and "fourthcol" should be removed
+        # Expected behavior: Attributes "firstcol", "fourthcol" and "fifthcol" will be removed
 
-        df = fake_temporal_series_dataset()
-        df_expected = df[:, [2,3]]
         mfd = SoleBase.MultiFrameDataset(df)
+        df_expected = df[:, [2,3]]
         mfd_expected = SoleBase.MultiFrameDataset(df_expected)
 
         vt = VarianceThreshold(0.09)
-        mfd_generated = SoleFeatures.apply(mfd, vt)
+        mfd_generated = SoleFeatures.apply(mfd, vt, normalize_function = SoleFeatures.minmax_normalize)
 
         @test SoleBase.isequal(mfd_expected, mfd_generated)
     end
+
+    @testset "Testing VarianceThreshold on temporal series of frame of MultiFrameDataset" begin
+        # test VarianceThreshold on temporal series of frame of MultiFrameDataset
+        # Variance threshold: 0.09
+        # Expected behavior: Attributes "firstcol" and "fourthcol" will be removed
+
+        mfd = SoleBase.MultiFrameDataset([[1,2,3,4],[5]], df)
+        df_expected = df[:, [2,3,5]]
+        mfd_expected = SoleBase.MultiFrameDataset([[1,2],[3]], df_expected)
+
+        vt = VarianceThreshold(0.09)
+        frame_num = 1
+        mfd_generated = SoleFeatures.apply(mfd, vt, frame_num, normalize_function = SoleFeatures.minmax_normalize)
+
+        @test SoleBase.isequal(mfd_expected, mfd_generated)
+    end
+
+    @testset "Testing VarianceRanking on temporal series of frame of MultiFrameDataset" begin
+        # test VarianceThreshold on temporal in MultiFrameDataset
+        # Top selected features: 2
+        # Expected behavior: Attributes "firstcol" and "fourthcol" will be removed
+
+        mfd = SoleBase.MultiFrameDataset(df)
+        df_expected = df[:, [2,3]]
+        mfd_expected = SoleBase.MultiFrameDataset(df_expected)
+
+        vr = VarianceRanking(2)
+        mfd_generated = SoleFeatures.apply(mfd, vr, normalize_function = SoleFeatures.minmax_normalize)
+
+        @test SoleBase.isequal(mfd_expected, mfd_generated)
+    end
+
+    @testset "Testing VarianceRanking on temporal series of frame of MultiFrameDataset" begin
+        # test VarianceRanking on temporal series of frame of MultiFrameDataset
+        # Top selected features: 2
+        # Expected behavior: Attributes "firstcol", "secondcol" and "fourthcol" will be removed
+
+        mfd = SoleBase.MultiFrameDataset([[1,2,3,4],[5]], df)
+        df_expected = df[:, [2,5]]
+        mfd_expected = SoleBase.MultiFrameDataset([[1],[2]], df_expected)
+
+        vr = VarianceRanking(1)
+        frame_num = 1
+        mfd_generated = SoleFeatures.apply(mfd, vr, frame_num, normalize_function = SoleFeatures.minmax_normalize)
+
+        @test SoleBase.isequal(mfd_expected, mfd_generated)
+    end
+
 
 end
