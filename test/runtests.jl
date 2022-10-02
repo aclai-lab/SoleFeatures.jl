411011a00c4303104990069d39a33defa0587c8e
PatrikCavina <patrik.cavina@edu.unife.it>
2022-10-02 17:07:21 +0000

    Tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,21 +6,23 @@ include("./test_function.jl")
 @testset "SoleFeatures.jl" begin
 
     df = fake_temporal_series_dataset()
-    nf = SoleFeatures.minmax_normalize_wrapper(0.0, 1.0)
+    ndf = SoleFeatures.minmax_normalize(df; min_quantile=0.0, max_quantile=1.0)
+    # nf = SoleFeatures.minmax_normalize_wrapper(0.0, 1.0)
 
     @testset "Testing VarianceThreshold on temporal series in MultiFrameDataset" begin
         # test VarianceThreshold on temporal series in MultiFrameDataset
         # Variance threshold: 0.09
         # Expected behavior: Attributes "firstcol", "fourthcol" and "fifthcol" will be removed
 
-        mfd = SoleBase.MultiFrameDataset(df)
-        df_expected = df[:, [2,3]]
-        mfd_expected = SoleBase.MultiFrameDataset(df_expected)
+        mfd = SoleBase.MultiFrameDataset(ndf)
+        dfe = ndf[:, [2,3]]
+        mfde = SoleBase.MultiFrameDataset(dfe)
 
         vt = VarianceThreshold(0.09)
-        mfd_generated = SoleFeatures.apply(mfd, vt; normalize_function = nf)
+        mfdg = SoleFeatures.apply(mfd, vt)
+        dfg = SoleBase.SoleDataset.data(mfdg)
 
-        @test SoleBase.isequal(mfd_expected, mfd_generated)
+        @test isequal(dfe, dfg)
     end
 
     @testset "Testing VarianceThreshold on temporal series of frame of MultiFrameDataset" begin
@@ -28,15 +30,15 @@ include("./test_function.jl")
         # Variance threshold: 0.09
         # Expected behavior: Attributes "firstcol" and "fourthcol" will be removed
 
-        mfd = SoleBase.MultiFrameDataset([[1,2,3,4],[5]], df)
-        df_expected = df[:, [2,3,5]]
-        mfd_expected = SoleBase.MultiFrameDataset([[1,2],[3]], df_expected)
+        mfd = SoleBase.MultiFrameDataset([[1,2,3,4],[5]], ndf)
+        dfe = ndf[:, [2,3,5]]
+        mfde = SoleBase.MultiFrameDataset([[1,2],[3]], dfe)
 
         vt = VarianceThreshold(0.09)
-        frame_num = 1
-        mfd_generated = SoleFeatures.apply(mfd, vt, frame_num; normalize_function = nf)
+        mfdg = SoleFeatures.apply(mfd, vt, 1)
+        dfg = SoleBase.SoleDataset.data(mfdg)
 
-        @test SoleBase.isequal(mfd_expected, mfd_generated)
+        @test isequal(dfe, dfg)
     end
 
     @testset "Testing VarianceRanking on temporal series of frame of MultiFrameDataset" begin
@@ -44,14 +46,15 @@ include("./test_function.jl")
         # Top selected features: 2
         # Expected behavior: Attributes "firstcol" and "fourthcol" will be removed
 
-        mfd = SoleBase.MultiFrameDataset(df)
-        df_expected = df[:, [2,3]]
-        mfd_expected = SoleBase.MultiFrameDataset(df_expected)
+        mfd = SoleBase.MultiFrameDataset(ndf)
+        dfe = ndf[:, [2,3]]
+        mfde = SoleBase.MultiFrameDataset(dfe)
 
         vr = VarianceRanking(2)
-        mfd_generated = SoleFeatures.apply(mfd, vr; normalize_function = nf)
+        mfdg = SoleFeatures.apply(mfd, vr)
+        dfg = SoleBase.SoleDataset.data(mfdg)
 
-        @test SoleBase.isequal(mfd_expected, mfd_generated)
+        @test isequal(dfe, dfg)
     end
 
     @testset "Testing VarianceRanking on temporal series of frame of MultiFrameDataset" begin
@@ -59,15 +62,15 @@ include("./test_function.jl")
         # Top selected features: 2
         # Expected behavior: Attributes "firstcol", "secondcol" and "fourthcol" will be removed
 
-        mfd = SoleBase.MultiFrameDataset([[1,2,3,4],[5]], df)
-        df_expected = df[:, [2,5]]
-        mfd_expected = SoleBase.MultiFrameDataset([[1],[2]], df_expected)
+        mfd = SoleBase.MultiFrameDataset([[1,2,3,4],[5]], ndf)
+        dfe = ndf[:, [2,5]]
+        mfde = SoleBase.MultiFrameDataset([[1],[2]], dfe)
 
         vr = VarianceRanking(1)
-        frame_num = 1
-        mfd_generated = SoleFeatures.apply(mfd, vr, frame_num; normalize_function = nf)
+        mfdg = SoleFeatures.apply(mfd, vr, 1)
+        dfg = SoleBase.SoleDataset.data(mfdg)
 
-        @test SoleBase.isequal(mfd_expected, mfd_generated)
+        @test isequal(dfe, dfg)
     end
 
     @testset "Testing VarianceThreshold on temporal series of frame of MultiFrameDataset" begin
--- test/test.jl
+++ test/test.jl
@@ -1,57 +1,6 @@
-using SoleFeatures
-using DynamicAxisWarping
+include("/home/painkiller/develop/SoleFeatures.jl/src/SoleFeatures.jl")
+using StatsBase
 
-include("./test_function.jl")
-
-function _compute_dtw(df::AbstractDataFrame; use_thread=false)::Array{Float64,2}
-    # maybe a better implmentation do dtw only on a vector of timeseries
-
-    nr, nc = size(df)
-    # number of rows in the result matrix
-    nrm = Int((nr * (nr - 1)) / 2)
-    # distances matrix
-    dist_matrix = Array{Float64,2}(undef, nrm, nc)
-    if (use_thread)
-        # computation of the dtw for each timeseries in a column for each attribute in df
-        Threads.@threads for cidx in 1:nc
-            idxm = 1
-            for iidx in 1:(nr-1)
-                for jidx in (iidx+1):nr
-                    # dtw returns cost and a set of indices (i1,i2) that align the two serie, so only cost (dtw(...)[1])
-                    # have to be extracted
-                    dist_matrix[idxm, cidx] = dtw(df[iidx, cidx], df[jidx, cidx])[1]
-                    idxm = idxm + 1
-                end
-            end
-        end
-    else
-        # computation of the dtw for each timeseries in a column for each attribute in df
-        for cidx in 1:nc
-            idxm = 1
-            for iidx in 1:(nr-1)
-                for jidx in (iidx+1):nr
-                    # dtw returns cost and a set of indices (i1,i2) that align the two serie, so only cost (dtw(...)[1])
-                    # have to be extracted
-                    dist_matrix[idxm, cidx] = dtw(df[iidx, cidx], df[jidx, cidx])[1]
-                    idxm = idxm + 1
-                end
-            end
-        end
-    end
-    return dist_matrix
-end
-
-res = BitVector()
-
-for i in 1:100
-    df1 = random_timeseries_df()
-    df2 = deepcopy(df1)
-    dtw_t = _compute_dtw(df1; use_thread=true)
-    dtw_nt = _compute_dtw(df2; use_thread=false)
-    ri = isequal(dtw_t, dtw_nt)
-    println("Finito: " * string(ri))
-    push!(res, ri)
-end
-
-println(res)
-println(all(==(true), res))
+mtrx = rand(4,4)
+cmtrx = cor(mtrx)
+SoleFeatures.findcorrelation(cmtrx)
