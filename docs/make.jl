4d6574748adce778812e2d40a8eb955715d9f5e9
giopaglia <24519853+giopaglia@users.noreply.github.com>
2025-03-26 13:06:37 +0100

    minor

--- docs/make.jl
+++ docs/make.jl
@@ -20,6 +20,7 @@ makedocs(;
 
 deploydocs(;
     repo = "github.com/aclai-lab/SoleFeatures.jl",
+    devbranch = "dev",
     target = "build",
     branch = "gh-pages",
     versions = ["main" => "main", "stable" => "v^", "v#.#", "dev" => "dev"],
